cbeeb3bf9220004789f97c252c4b6a603f9b11c2
ronanodriscoll <ronanodriscoll@c7b9e1fc-d365-11dd-8ca8-e3ff79f713b6>
2008-12-27 01:18:17 +0000

    initial checkin

---
+++ src/com/ronanodriscoll/SplashScreen.java
@@ -0,0 +1,104 @@
+package com.ronanodriscoll;
+/* 
+ * PROJECT: Filippo Drawing Program.
+ * --------------------------------------------------------------------------------
+ * The MIT License
+ * Copyright (c) 2008 Ronan O'Driscoll
+ * ronanodriscoll(at)gmail.com
+ * http://code.google.com/p/filippo/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ */
+/**
+ * Copied from:
+ * Java Swing, 2nd Edition
+ * By Marc Loy, Robert Eckstein, Dave Wood, James Elliott, Brian Cole
+ * ISBN: 0-596-00408-7
+ * Publisher: O'Reilly 
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+
+@SuppressWarnings("serial")
+public class SplashScreen extends JWindow {
+  private int duration;
+
+  public SplashScreen(int d) {
+    duration = d;
+  }
+
+  // A simple little method to show a title screen in the center
+  // of the screen for the amount of time given in the constructor
+  public void showSplash() {
+    JPanel content = (JPanel) getContentPane();
+    content.setBackground(Color.white);
+
+    // Set the window's bounds, centering the window
+    int width = 420;
+    int height = 320;
+    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (screen.width - width) / 2;
+    int y = (screen.height - height) / 2;
+    setBounds(x, y, width, height);
+
+    // Build the splash screen
+    JLabel label = new JLabel(new ImageIcon(
+    		SplashScreen.class.getResource("resources/splash.png")));
+    JLabel copyrt = new JLabel("Released under the MIT License",
+        JLabel.CENTER);
+    copyrt.setFont(new Font("Sans-Serif", Font.BOLD, 12));
+    content.add(label, BorderLayout.CENTER);
+    content.add(copyrt, BorderLayout.SOUTH);
+    Color oraRed = new Color(156, 20, 20, 255);
+    content.setBorder(BorderFactory.createLineBorder(oraRed, 10));
+
+    // Display it
+    setVisible(true);
+
+    // Wait a little while, maybe while loading resources
+    try {
+      Thread.sleep(duration);
+    } catch (Exception e) {
+    }
+
+    setVisible(false);
+  }
+
+  public void showSplashAndExit() {
+    showSplash();
+    System.exit(0);
+  }
+
+  public static void main(String[] args) {
+    // Throw a nice little title page up on the screen first
+    SplashScreen splash = new SplashScreen(10000);
+    // Normally, we'd call splash.showSplash() and get on with the program.
+    // But, since this is only a test...
+    splash.showSplashAndExit();
+  }
+}
